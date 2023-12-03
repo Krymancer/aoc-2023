0fc1287a38e08c56c8a3b96062b5dfc3db54c786
José Nascimento <141747002+jejunior3@users.noreply.github.com>
2023-12-02 21:11:47 -0300

    aoc day 2 second part

--- 02/main.js
+++ 02/main.js
@@ -1,31 +1,55 @@
 const input = require('fs').readFileSync('input.txt', 'utf8').split('\n');
 
-const bag = {
-  'red': 12,
-  'green': 13,
-  'blue': 14 
-};
+// const bag = {
+//   'red': 12,
+//   'green': 13,
+//   'blue': 14 
+// };
+//
+// const sum = input.map((line) => {
+//   const [gameId, gameData] = line.split(':');
+//   const id = gameId.split(' ')[1];
+
+//   const turns = gameData.split(';');
+
+//   let ok = true;
+
+//   turns.map((turn) => {
+//     turn.split(',').map((cube) => {
+//       const [count, color] = cube.trim().split(' ');
+//       if(bag[color] < count) {
+//         ok = false;
+//         return;
+//       }
+//     });
+//     if(!ok) return;
+//   });
+
+//   return  ok  ? parseInt(id) : 0;
+// }).reduce((a, b) => a + b);
+
+// console.log(sum)
+
 
 const sum = input.map((line) => {
   const [gameId, gameData] = line.split(':');
-  const id = gameId.split(' ')[1];
-
   const turns = gameData.split(';');
 
-  let ok = true;
+  let bag = {
+    'red': 0,
+    'green': 0,
+    'blue': 0
+  };
 
-  turns.map((turn) => {
+  let powers = turns.map((turn) => {
     turn.split(',').map((cube) => {
       const [count, color] = cube.trim().split(' ');
-      if(bag[color] < count) {
-        ok = false;
-        return;
-      }
+      bag[color] = Math.max(bag[color], parseInt(count));
     });
-    if(!ok) return;
   });
 
-  return  ok  ? parseInt(id) : 0;
+
+  return bag.red * bag.green * bag.blue;
 }).reduce((a, b) => a + b);
 
 console.log(sum)
